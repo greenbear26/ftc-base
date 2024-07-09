639522568d4b5cc99ac848f176808ea80341d262
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2024-07-09 15:06:23 -0400

    Changed TransitionBuilder

--- state-machine/src/main/java/team/techtigers/statemachine/TransitionBuilder.java
+++ state-machine/src/main/java/team/techtigers/statemachine/TransitionBuilder.java
@@ -5,7 +5,7 @@ import java.util.function.BooleanSupplier;
 public class TransitionBuilder {
     private final StateMachine stateMachine;
     private final String currentState;
-    private BooleanSupplier condition;
+    private String nextState;
 
     /**
      * Initializes a new TransitionBuilder
@@ -22,27 +22,27 @@ public class TransitionBuilder {
      * Adds a condition to the transition
      *
      * @param condition the condition that must be met for the state to transition
-     * @return the transition builder to allow for method chaining
-     */
-    public TransitionBuilder when(BooleanSupplier condition) {
-        this.condition = condition;
-
-        return this;
-    }
-
-    /**
-     * Adds the next state to the transition, which is then added to the state machine
-     *
-     * @param nextState the name of the next state
      * @return the state machine to allow for method chaining
      */
-    public StateMachine to(String nextState) {
-        if (condition == null) {
-            throw new IllegalStateException("Condition must be set (use when)");
+    public StateMachine when(BooleanSupplier condition) {
+        if (nextState == null) {
+            throw new IllegalStateException("Condition must be set (use to)");
         }
 
         stateMachine.addCondition(currentState, new Transition(condition, nextState));
 
         return stateMachine;
+    }
+
+    /**
+     * Adds the next state to the transition
+     *
+     * @param nextState the name of the next state
+     * @return the transition builder to allow for method chaining
+     */
+    public TransitionBuilder to(String nextState) {
+        this.nextState = nextState;
+
+        return this;
     }
 }
